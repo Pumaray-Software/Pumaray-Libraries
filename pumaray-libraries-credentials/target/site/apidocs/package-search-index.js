914f6f71d4f91ddbadd7a44d2fe485483158ec99
randy <tiras@pumaray.com>
2021-02-10 17:20:58 +0100

    Updates

---
+++ pumaray-libraries-credentials/target/site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.pumaray.lib.cred.user"},{"l":"com.pumaray.lib.cred.user.impl"},{"l":"com.pumaray.lib.cred.user.role.impl"},{"l":"com.pumaray.lib.cred.user.role.type.impl"}]
