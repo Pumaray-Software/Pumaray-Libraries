92884825d70100d2879eb2d6034157aa0e618d40
pumaraysoftware@gmail.com <pumaraysoftware@gmail.com>
2022-03-10 14:45:11 +0100

    design

--- pumaray-libraries-credentials/target/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.pumaray.lib.cred.user"},{"l":"com.pumaray.lib.cred.user.impl"},{"l":"com.pumaray.lib.cred.user.role.impl"},{"l":"com.pumaray.lib.cred.user.role.type.impl"}]
